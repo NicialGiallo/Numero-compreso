e8ff77f976756ff531e690eb280cb6929cf84c34
NicialGiallo <114949965+NicialGiallo@users.noreply.github.com>
2022-10-04 12:19:59 +0200

    Add files via upload

---
+++ numero compreso.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "Italian");
+	float A,B,C;
+	cout << "Inserisci 3 valori" << endl;
+	cin >> A >> B >> C;
+	
+	if(A<C and C<B) {
+		cout << C << " è compreso tra " << A << " e " << B << endl;
+	}
+	 else if(A>C and C>B) {
+		cout << C << " è compreso tra " << A << " e " << B << endl;
+	}
+	else
+	{
+		cout << C << " non è compreso tra " << A << " e " << B << endl;
+	}
+	return 0;
+}
